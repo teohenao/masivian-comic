4385942464a07af4c77909e527469d384a7af17c
mateo henao rodriguez <teohenao@mateos-MacBook-Air.local>
2020-05-05 20:51:45 -0500

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from './component/principal/principal.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:'/inicio',pathMatch:'full'},
+  {path:'inicio',component:PrincipalComponent},
+  { path: '**', component:PrincipalComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './component/principal/principal.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,

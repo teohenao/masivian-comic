5c8afc3abe4124d8bd7ebff7c3844573f341612c
mateo henao rodriguez <teohenao@mateos-MacBook-Air.local>
2020-05-07 10:21:22 -0500

    random comics

--- src/app/component/principal/principal.component.ts
+++ src/app/component/principal/principal.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ComicsService } from 'src/app/services/comics.service';
 import { Comic } from 'src/app/models/comic';
+import Swal from 'sweetalert2';
+
+
+declare const STABLE_FEATURE: boolean;
+declare const EXPERIMENTAL_FEATURE: boolean;
 
 @Component({
   selector: 'app-principal',
@@ -9,16 +14,36 @@ import { Comic } from 'src/app/models/comic';
 })
 export class PrincipalComponent implements OnInit {
 
+  stableFeature: string;
+  experimentalFeature: string;
   comic:Comic;
+  rating:boolean=false;
 
   constructor(private comicsService:ComicsService) { }
 
   ngOnInit() {
-    this.comic = new Comic();
-      this.comicsService.getComit(611).subscribe(res=>{
-       this.comic = res;
-       console.log(this.comic)
-      })
+    this.comicRandom()
+    this.stableFeature = STABLE_FEATURE ? 'Stable feature enabled' : 'Stable feature disabled';
+    this.experimentalFeature = EXPERIMENTAL_FEATURE ? 'Experimental feature enabled' : 'Experimental feature disabled';
   }
 
+  ratingComic(r:number){
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'You have correctly rated the comic '+this.comic.title +' with '+r+' star',
+      showConfirmButton: false,
+      timer: 3000
+    })
+  }
+
+  comicRandom(){
+    this.rating = false;
+    let random= Math.floor(Math.random() * 2302) + 1  
+    this.comic = new Comic();
+      this.comicsService.getComit(random).subscribe(res=>{
+       this.comic = res;
+       this.rating = true;
+      })
+  }
 }
